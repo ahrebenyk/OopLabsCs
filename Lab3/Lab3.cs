176319cb1e4c3180a74b94a71fa66e5f36ecd763
Artem Grebenyk <a.hrebenyk@kitu.nau.edu.ua>
2026-02-23 20:58:10 +0200

    add lab 3,4

---
+++ Lab3/Lab3.cs
@@ -0,0 +1,155 @@
+﻿using System.Text;
+using static System.Console;
+
+namespace Lab3;
+
+public class Lab3
+{
+    public static void Main()
+    {
+        OutputEncoding = Encoding.UTF8;
+        WriteLine("Введіть кількість рядків масиву");
+        var rows = ReadInt();
+        WriteLine("Введіть мін кількість елементів в рядку масиву");
+        var minRowSize = ReadInt();
+        WriteLine("Введіть макс кількість елементів в рядку масиву");
+        var maxRowSize = ReadInt();
+        
+        var sourceArray = InitArray(rows, minRowSize, maxRowSize);
+        WriteLine("Початковий масив:");
+        PrintArray(sourceArray);
+
+        var flatArray = GetFlatArray(sourceArray);
+        WriteLine("Одновимірний масив:");
+        PrintArray(flatArray);
+        
+        SortArray(flatArray);
+        WriteLine("Відсортований масив:");
+        PrintArray(flatArray);
+
+        var matrix = FlatArrayToMatrix(flatArray);
+        WriteLine("Відсортована таблиця:");
+        PrintArray(matrix);
+    }
+
+    private static int ReadInt()
+    {
+        return int.Parse(ReadLine());
+    }
+
+    private static int[][] InitArray(int rows, int minRowSize, int maxRowSize)
+    {
+        var array = new int[rows][];
+        var numOfElements = 0;
+        for (var i = 0; i < rows; i ++)
+        {
+            var rowSize = i == rows - 1 
+                ? GetNextSquare(numOfElements) - numOfElements
+                : Random.Shared.Next(minRowSize, maxRowSize + 1);
+
+            numOfElements += rowSize;
+            array[i] = new int[rowSize];
+            for (var j = 0; j < rowSize; j++)
+            {
+                array[i][j] = Random.Shared.Next(0, 100);
+            }
+        }
+        return array;
+    }
+
+    private static void PrintArray(int[][] array)
+    {
+        foreach (var row in array)
+        {
+            PrintArray(row);
+        }
+    }
+    
+    private static void PrintArray(int[] array)
+    {
+        WriteLine(string.Join(" ", array));
+    }
+    
+    private static void PrintArray(int[,] array)
+    {
+        var n = array.GetLength(0);
+        var m = array.GetLength(1);
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < m; j++)
+            {
+                Write(array[i, j] + " "); 
+            }
+            WriteLine();
+        }
+    }
+    
+    private static int GetNextSquare(int n)
+    {
+        var nextRoot = (int) Math.Floor(Math.Sqrt(n)) + 1;
+        return nextRoot * nextRoot;
+    }
+
+    private static int GetTotalElements(int[][] array)
+    {
+        var totalElements = 0;
+        foreach (var row in array)
+        {
+            totalElements += row.Length;
+        }    
+        return totalElements;
+    }
+        
+    private static int[] GetFlatArray(int[][] array)
+    {
+        int[] flatArray = new int[GetTotalElements(array)];
+        var index = 0;
+
+        for (var i = 0; i < array.Length; i++)
+        {
+            for (var j = 0; j < array[i].Length; j++)
+            {
+                flatArray[index] = array[i][j];
+                index++;
+            }
+        }
+        return flatArray;
+    }
+        
+    private static void SortArray(int[] array)
+    {
+        var n = array.Length;
+        for (var i = 0; i < n - 1; i++)
+        {
+            var minIndex = i;
+            for (var j = i + 1; j < n; j++)
+            {
+                if (array[j] < array[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            (array[minIndex], array[i]) = (array[i], array[minIndex]);
+        }    
+    }
+        
+    private static int[,] FlatArrayToMatrix(int[] array)
+    {
+        var n = (int) Math.Sqrt(array.Length);
+        var matrix = new int[n, n];
+            
+        var index = 0;
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+               matrix[i, j] = array[index]; 
+               index++;
+            }
+        }
+
+        return matrix;
+    }
+        
+}
---
+++ Lab4/Lab4.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using static System.Console;
+
+namespace Lab4;
+
+public class Lab4
+{
+    public static void Main()
+    {
+        OutputEncoding = Encoding.UTF8;
+        WriteLine("Введіть кількість рядків масиву");
+        var rows = int.Parse(ReadLine());
+        WriteLine("Введіть кількість колонок масиву");
+        var cols = int.Parse(ReadLine());
+        
+        var sourceArray = InitArray(rows, cols);
+        WriteLine("Десяткові значення рядків:");
+        PrintRowDecimalValues(sourceArray);
+    }
+
+    private static int[,] InitArray(int rows, int cols)
+    {
+        var array = new int[rows, cols];
+        for (var i = 0; i < rows; i ++)
+        {
+            for (var j = 0; j < cols; j++)
+            {
+                array[i,j] = Random.Shared.Next(0, 2);
+            }
+        }
+        return array;
+    }
+    
+    private static void PrintRowDecimalValues(int[,] array)
+    {
+        var n = array.GetLength(0);
+        var m = array.GetLength(1);
+        for (var i = 0; i < n; i++)
+        {
+            var decimalValue = 0;
+            for (var j = 0; j < m; j++)
+            {
+                Write(array[i, j] + " ");
+                if (array[i, j] > 0)
+                {
+                    decimalValue += (int) Math.Pow(2, m - j - 1);
+                }
+            }
+            Write("\t" + decimalValue + " ");
+            WriteLine();
+        }
+    }
+        
+}
