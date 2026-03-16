c148fccdb3c7213d8e994a97ce78c135f1aba820
Artem Grebenyk <a.hrebenyk@kitu.nau.edu.ua>
2026-03-16 14:08:42 +0200

    lab 7-8

---
+++ Lab7/Lab7.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using static System.Console;
+
+namespace Lab4;
+
+public class Lab7
+{
+    public static void Main()
+    {
+        WriteLine("enter real and imaginary part of complex1");
+        var r1 = double.Parse(ReadLine());
+        var i1 = double.Parse(ReadLine());
+        var complex1 = new Complex(r1, i1);
+        
+        WriteLine("enter real and imaginary part of complex2");
+        var r2 = double.Parse(ReadLine());
+        var i2 = double.Parse(ReadLine());
+        var complex2 = new Complex(r2, i2);
+
+        WriteLine("complex1 = " + complex1);
+        WriteLine("complex2 = " + complex2);
+        WriteLine("complex1 + complex2 = " + complex1.Add(complex2));
+        WriteLine("complex1 - complex2 = " + complex1.Subtract(complex2));
+        WriteLine("complex1 * complex2 = " + complex1.Multiply(complex2));
+    }
+    
+}
