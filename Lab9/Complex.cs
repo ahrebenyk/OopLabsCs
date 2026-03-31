e05773b4d7e5fedf566dd4186074f606c16b0b9a
Artem Grebenyk <a.hrebenyk@kitu.nau.edu.ua>
2026-03-31 22:45:25 +0300

    add lab9

---
+++ Lab9/Complex.cs
@@ -0,0 +1,54 @@
+﻿namespace Lab4;
+
+public class Complex
+{
+    private double _real;
+    private double _imaginary;
+
+    public Complex()
+    {
+        _real = 0;
+        _imaginary = 0;
+    }
+
+    public Complex(double real, double imaginary)
+    {
+        _real = real;
+        _imaginary = imaginary;
+    }
+
+    public Complex(double real)
+    {
+        _real = real;
+    }
+
+    public Complex(Complex other)
+    {
+        _real = other._real;
+        _imaginary = other._imaginary;
+    }
+
+    public Complex Add(Complex other)
+    {
+        return new Complex(_real + other._real, _imaginary + other._imaginary);
+    }
+
+    public Complex Subtract(Complex other)
+    {
+        return new Complex(_real - other._real, _imaginary - other._imaginary);
+        
+    }
+
+    public Complex Multiply(Complex other)
+    {
+        var resReal = _real * other._real - _imaginary * other._imaginary;
+        var resImaginary = _real * other._imaginary + _imaginary * other._real;
+        return new Complex(resReal, resImaginary);
+    }
+
+    public override string ToString()
+    {
+        var sign = _imaginary >= 0 ? "+" : "-";
+        return $"{_real} {sign} {Math.Abs(_imaginary)}i";
+    }
+}
---
+++ Lab9/Lab9.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using static System.Console;
+
+namespace Lab4;
+
+public class Lab9
+{
+    public static void Main()
+    {
+        var defaultValue = new Complex();
+        WriteLine("defaultValue = " + defaultValue);
+
+        var realValue = new Complex(11.0);
+        WriteLine("real value = " + realValue);
+        
+        var complexValue = new Complex(11.0, 23.1);
+        WriteLine("initialized value = " + complexValue);
+        
+        var copiedValue = new Complex(complexValue);
+        WriteLine("Copied value = " + copiedValue);
+   }
+    
+}
